f07972dd1bb4553ed47948321486f6453b896083
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-08-14 07:30:40 +0500

    User Axelalvess Alvess saved changes to main.py at 08/14/2025, 02:30:39 UTC time.

--- e5ab7823-375a-4544-8744-737ee7f127ee/main.py
+++ e5ab7823-375a-4544-8744-737ee7f127ee/main.py
@@ -1,1 +1,23 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA, STDEV
+from surmount.data import Asset
+import pandas as pd
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.ticker = "AAPL"
+
+    @property
+    def interval(self):
+        # Analysis interval
+        return "1day"
+
+    @property
+    def assets(self):
+        # Assets to include in the strategy
+        return [self.ticker]
+
+    @property
+    def data(self):
+        # Additional data requirements are listed here
+        # Observing the base example, only
